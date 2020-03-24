266db1330e846cd225913c178666dc744e8c6599
AdamWen95 <739513317@qq.com>
2020-03-24 23:44:46 +0800

    '首页第二次提交页面滚动'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,45 +1,136 @@
 <template>
 <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+
+<scroll class="content">
+<home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
+</scroll>
+    
 </div>
 </template>
 
 <script>
+//导入公用组件
 import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
+import Scroll from 'components/common/scroll/Scroll'
+
+//导入home的子组件
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
 
-import {getHomeMultidata} from 'network/home'
+//导入网络请求封装好的函数
+import {getHomeMultidata, getHomeGoods} from 'network/home'
 
 export default {
     name: "Home",
     data() {
         return {
             banners: [],
-            recommends: []
+            recommends: [],
+            goods: {
+                //保存tab-control的数据
+                'pop': {page: 0, list: []},
+                'new': {page: 0, list: []},
+                'sell': {page: 0, list: []},
+            },
+            //当前选中的标签
+            currentType: 'pop'
         }
     },
     components: {
         NavBar,
+        TabControl,
+        Scroll,
+        GoodsList,
         HomeSwiper,
-        RecommendView
+        RecommendView,
+        FeatureView
+    },
+    computed: {
+        showGoods() {
+            return this.goods[this.currentType].list
+        }
     },
     created() {
-        getHomeMultidata().then(res => {
-            console.log(res);
+        //created内部尽量简洁，方法用methods包装，在这里直接调用methods的方法就行
+        //请求轮播图和推荐的数据
+        this.getHomeMultidata()
+
+        //请求tab-control的商品数据
+        this.getHomeGoods('pop')
+        this.getHomeGoods('new')
+        this.getHomeGoods('sell')
+    },
+    methods: {
+        tabClick(index) {
+            switch(index) {
+                case 0: this.currentType = 'pop';
+                break;
+                case 1: this.currentType = 'new';
+                break;
+                case 2: this.currentType = 'sell';
+                break;
+            }
+        },
+        getHomeMultidata() {
+            getHomeMultidata().then(res => {
+            // console.log(res);
             this.banners = res.data.banner.list;
             this.recommends = res.data.recommend.list;
         })
+        },
+        getHomeGoods(type) {
+            //每次请求的是下一页的数据
+            const page = this.goods[type].page + 1;
+            getHomeGoods(type, page).then(res => {
+                //使用了...运算符展开数组，因为push可以传入多个值
+                this.goods[type].list.push(...res.data.list);
+                //添加完数据后页数+1
+                this.goods[type].page += 1
+            })
+        }
     }
 }
 </script>
 
 <style scoped>
+#home {
+    padding-top: 44px;
+    position: relative;
+    /* 子盒子加了绝对定位，脱离标准流，父盒子是被撑开的->父盒子高度变0，需要给父盒子自己加一个高度，（100vh->视口高度） */
+    height: 100vh;
+}
+
 .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
 }
 
+.tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+}
+
+.content {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+}
 </style>
