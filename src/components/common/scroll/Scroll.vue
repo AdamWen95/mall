5245a48cb25ad5941bc995fbd3696868471fb0ad
AdamWen95 <739513317@qq.com>
2020-03-26 22:13:45 +0800

    '首页第三次提交'

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -11,16 +11,48 @@ import BScroll from 'better-scroll'
 
 export default {
     name: "Scroll",
+    props: {
+        probeType: {
+            type: Number,
+            defult: 0
+        },
+        pullUPLoad: {
+            type: Boolean,
+            defult: false
+        }
+    },
     data() {
         return {
             scroll: null
         }
     },
     mounted() {
+        //创建BScroll对象
         this.scroll = new BScroll(this.$refs.wrapper, {
-
+            //scroll里面除了button之外其他元素的点击事件如果要被监听到需要设置true
+            click: true,
+            probeType: this.probeType,
+            pullUPLoad: this.pullUPLoad
+        }),
+        //监听滚动的位置
+        this.scroll.on('scroll', (position) => {
+            this.$emit('scroll', position)
+        }),
+        //监听上拉事件
+        this.scroll.on('pullingUp', () => {
+            this.$emit('pullingUp')
         })
-    }
+    },
+    methods: {
+        //父组件通过$refs拿到子组件并调用子组件已经封装的方法会比直接在父组件调用方法更好，因此对scroll的方法做一层封装
+        //scrollTo第三个参数代表到达目标坐标所需的时间，这里默认传入300ms
+        scrollTo(x, y, time=300) {
+            this.scroll.scrollTo(x, y, time)
+        },
+        finishPullUp() {
+            this.scroll.finishPullUp()
+        }
+    },
 }
 </script>
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,13 +2,15 @@
 <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
 
-<scroll class="content">
+<scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll" :pull-up-load="true" @pullingUp="loadMore">
 <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
     <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
     <goods-list :goods="showGoods"></goods-list>
 </scroll>
+
+<back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
     
 </div>
 </template>
@@ -19,6 +21,7 @@ import NavBar from 'components/common/navbar/NavBar'
 import TabControl from 'components/content/tabControl/TabControl'
 import GoodsList from 'components/content/goods/GoodsList'
 import Scroll from 'components/common/scroll/Scroll'
+import BackTop from 'components/content/backTop/BackTop'
 
 //导入home的子组件
 import HomeSwiper from './childComps/HomeSwiper'
@@ -41,13 +44,15 @@ export default {
                 'sell': {page: 0, list: []},
             },
             //当前选中的标签
-            currentType: 'pop'
+            currentType: 'pop',
+            isShowBackTop: false
         }
     },
     components: {
         NavBar,
         TabControl,
         Scroll,
+        BackTop,
         GoodsList,
         HomeSwiper,
         RecommendView,
@@ -84,7 +89,7 @@ export default {
             // console.log(res);
             this.banners = res.data.banner.list;
             this.recommends = res.data.recommend.list;
-        })
+            })
         },
         getHomeGoods(type) {
             //每次请求的是下一页的数据
@@ -93,8 +98,19 @@ export default {
                 //使用了...运算符展开数组，因为push可以传入多个值
                 this.goods[type].list.push(...res.data.list);
                 //添加完数据后页数+1
-                this.goods[type].page += 1
+                this.goods[type].page += 1;
+                //通过调用子组件的finishPullUp来使下次下拉能再一次加载更多
+                this.$refs.scroll.finishPullUp()
             })
+        },
+        backClick() {
+            this.$refs.scroll.scrollTo(0, 0, 500)
+        },
+        contentScroll(position) {
+            this.isShowBackTop = (-position.y) > 1000;
+        },
+        loadMore() {
+            this.getHomeGoods(this.currentType)
         }
     }
 }
@@ -104,7 +120,7 @@ export default {
 #home {
     padding-top: 44px;
     position: relative;
-    /* 子盒子加了绝对定位，脱离标准流，父盒子是被撑开的->父盒子高度变0，需要给父盒子自己加一个高度，（100vh->视口高度） */
+    /* 子盒子.content加了绝对定位，脱离标准流，父盒子是被撑开的->父盒子高度变0，需要给父盒子自己加一个高度，（100vh->视口高度） */
     height: 100vh;
 }
 
